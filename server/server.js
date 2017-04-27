17fe2a1c2a7da27bfba6bc769dd0a33ffe58d44e
yanolsh <yanolsh@bu.edu>
2017-04-26 20:58:16 -0400

    added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
@@ -28,9 +28,34 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     });
 });
+
+//GET /todos/12345
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({ todo });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
+
+
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
+
+
+
 
 module.exports = { app };
 // var newTodo = new Todo({
