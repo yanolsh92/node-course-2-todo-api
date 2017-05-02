3cbb06d8983eabf36c0569042f7cb9c82752a47d
yanolsh <yanolsh@bu.edu>
2017-05-01 22:36:59 -0400

    Add DELETE /users/me/token

--- server/models/user.js
+++ server/models/user.js
@@ -48,6 +48,15 @@ UserSchema.methods.toJSON = function() {
     return _.pick(userObject, ['_id', 'email']);
 };
 
+UserSchema.methods.removeToken = function(token) {
+    var user = this;
+    return user.update({
+        $pull: {
+            tokens: { token }
+        }
+    });
+};
+
 UserSchema.statics.findByToken = function(token) {
     var User = this;
     var decoded;
